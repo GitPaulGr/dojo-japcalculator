a19df82bc940266d442ada7ab820cf524f8a0b48
PaulGr <paul.grardel@student-cs.fr>
2022-10-18 16:34:34 +0200

    Reset button

--- src/pages/Calculator/Calculator.tsx
+++ src/pages/Calculator/Calculator.tsx
@@ -51,6 +51,10 @@ export const CalculatorPage: React.FC<Props> = (props: Props) => {
     });
   };
 
+  const reset = () => () => {
+    setValues({})
+  };
+
   const result = Object.keys(values).reduce((acc: number, itemName: string) => {
     return acc + values[itemName].kcal;
   }, 0);
@@ -80,6 +84,11 @@ export const CalculatorPage: React.FC<Props> = (props: Props) => {
           <div className={classes.buttonContainer}>
             <Button variant="contained" onClick={sendData(result)}>
               {result} Kcal
+            </Button>
+          </div>
+          <div className={classes.buttonContainer}>
+            <Button variant="contained" onClick={reset()}>
+              Reset
             </Button>
           </div>
         </div>
